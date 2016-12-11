cef7b357f7e2fe44c7afce7ebc77c870f02a7557
Isuru Dilshan <isurur@wso2telco.com>
2016-12-11 08:24:15 +0530

    1. Removed unwanted files 2. Completed xml parsing

--- bin/main.py
+++ bin/main.py
@@ -1,45 +1,56 @@
 import sys
 import os
 import xml.etree.ElementTree as ET
-from collections import defaultdict
 
 projects = ET.parse('settings.xml')
 
-data_dict = defaultdict(list)
+project_details = {}
+artifacts_map = {}
+projects_map = {}
 
-project = projects.find('project')
-name = ET.Element(project.find('name'))
-git_url = ET.Element(project.find('git-url'))
-is_sync = ET.Element(project.find('sync'))
-branch = ET.Element(project.find('sync'))
-source_dir = ET.Element(project.find('source-dir'))
-deploy_dir = ET.Element(project.find('deploy-dir'))
-artifacts = ET.ElementTree(project.find('artifacts'))
-available_artifacts = artifacts.findall('artifact')
+project_index = 0
+for project in projects.iter('project'):
 
-for artifact in available_artifacts:
-    artifact = ET.ElementTree(artifact)
-    properties = artifact.find('properties')
+    name = ET.Element(project.find('name'))
+    git_url = ET.Element(project.find('git-url'))
+    is_sync = ET.Element(project.find('sync'))
+    branch = ET.Element(project.find('sync'))
+    source_dir = ET.Element(project.find('source-dir'))
+    deploy_dir = ET.Element(project.find('deploy-dir'))
+    artifacts = ET.ElementTree(project.find('artifacts'))
 
-    property_details = {}
+    artifacts_map = {}
 
-    for property_value in properties:
-        property_value = ET.ElementTree(property_value)
+    artifact_index = 0
+    for artifact in artifacts.iter('artifact'):
 
-        property_details['filename'] = property_value.find('filename').text
-        property_details['path'] = property_value.find('path').text
+        artifact = ET.ElementTree(artifact)
+        properties = artifact.find('properties')
 
-    artifact_details = {'name': artifact.find('name').text,
-                        'type': artifact.find('type').text,
-                        'source-path': artifact.find('source-path').text,
-                        'deploy-path': artifact.find('deploy-path').text,
-                        'properties': property_details}
-    print(artifact_details)
+        property_details = {}
 
-print(name.tag.text)
-# print(root[0][6][0])
-# for child in root:
-#     print(child.tag)
-#     xml[root.tag] = child.tag
+        for property_value in properties:
+            property_value = ET.ElementTree(property_value)
 
-# print(xml)
+            property_details['filename'] = property_value.find('filename').text
+            property_details['path'] = property_value.find('path').text
+
+        artifact_details = {'name': artifact.find('name').text,
+                            'type': artifact.find('type').text,
+                            'source-path': artifact.find('source-path').text,
+                            'deploy-path': artifact.find('deploy-path').text,
+                            'properties': property_details}
+        artifacts_map['artifact_' + repr(artifact_index)] = artifact_details
+        artifact_index += 1
+
+    project_details['name'] = name.tag.text
+    project_details['git-url'] = git_url.tag.text
+    project_details['sync'] = is_sync.tag.text
+    project_details['branch'] = branch.tag.text
+    project_details['source-dir'] = source_dir.tag.text
+    project_details['deploy-dir'] = deploy_dir.tag.text
+    project_details['artifacts'] = artifacts_map
+
+    projects_map['project_' + repr(project_index)] = project_details
+    project_index += 1
+print(projects_map)

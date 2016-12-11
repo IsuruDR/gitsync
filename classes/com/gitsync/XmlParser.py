4a3748e23124862f8478d8806c402d194d7a449e
Isuru Dilshan <isurur@wso2telco.com>
2016-12-11 08:53:58 +0530

    Fixed Xml Parsing

---
+++ classes/com/gitsync/XmlParser.py
@@ -0,0 +1,59 @@
+import xml.etree.ElementTree as ET
+
+
+class XmlParser:
+    @staticmethod
+    def parse_xml(file_path):
+        projects = ET.parse(file_path)
+
+        projects_map = {}
+
+        project_index = 0
+        for project in projects.iter('project'):
+
+            project_details = {}
+
+            name = ET.Element(project.find('name'))
+            git_url = ET.Element(project.find('git-url'))
+            is_sync = ET.Element(project.find('sync'))
+            branch = ET.Element(project.find('branch'))
+            source_dir = ET.Element(project.find('source-dir'))
+            deploy_dir = ET.Element(project.find('deploy-dir'))
+            artifacts = ET.ElementTree(project.find('artifacts'))
+
+            artifacts_map = {}
+
+            artifact_index = 0
+            for artifact in artifacts.iter('artifact'):
+
+                artifact = ET.ElementTree(artifact)
+                properties = artifact.find('properties')
+
+                property_details = {}
+
+                if properties is not None:
+                    for property_value in properties:
+                        property_value = ET.ElementTree(property_value)
+
+                        property_details['filename'] = property_value.find('filename').text
+                        property_details['path'] = property_value.find('path').text
+
+                    artifact_details = {'name': artifact.find('name').text,
+                                        'type': artifact.find('type').text,
+                                        'source-path': artifact.find('source-path').text,
+                                        'deploy-path': artifact.find('deploy-path').text,
+                                        'properties': property_details}
+                    artifacts_map['artifact_' + repr(artifact_index)] = artifact_details
+                    artifact_index += 1
+
+            project_details['name'] = name.tag.text
+            project_details['git-url'] = git_url.tag.text
+            project_details['sync'] = is_sync.tag.text
+            project_details['branch'] = branch.tag.text
+            project_details['source-dir'] = source_dir.tag.text
+            project_details['deploy-dir'] = deploy_dir.tag.text
+            project_details['artifacts'] = artifacts_map
+
+            projects_map['project_' + repr(project_index)] = project_details
+            project_index += 1
+        return projects_map

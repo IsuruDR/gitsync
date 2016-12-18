e4cb1bc5a139df0ec2d812832cedee9ac4a71ced
Isuru Dilshan <isurur@wso2telco.com>
2016-12-18 09:26:04 +0530

    Added war file processing

--- bin/main.py
+++ bin/main.py
@@ -5,6 +5,9 @@ import subprocess
 import sys
 import json
 from pprint import pprint
+import logging
+
+logging.basicConfig(filename='../logs/info.log', level=logging.DEBUG)
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -36,11 +39,10 @@ for project_index in range(number_of_projects):
         pprint(artifact_data[artifact_index]['source-path'])
         artifact_source_path = artifact_data[artifact_index]['source-path']
         artifact_deploy_path = artifact_data[artifact_index]['deploy-path']
-
-        path_to_artifact = os.path.join(project_home, artifact_source_path)
-        os.chdir(path_to_artifact)
-
-        # subprocess.call(["mvn clean install"], shell=True)
+        artifact_final_name = artifact_data[artifact_index]['final-name']
+        artifact_version = ''
+        if 'version' in artifact_data[artifact_index]:
+            artifact_version = artifact_data[artifact_index]['version']
 
         if 'properties' in artifact_data[artifact_index]:
             property_data = artifact_data[artifact_index]['properties']
@@ -52,37 +54,41 @@ for project_index in range(number_of_projects):
 
                 property_source_full_path = os.path.join(project_home, artifact_source_path, property_source_path,
                                                          property_file_name)
-                property_deploy_full_path = os.path.join(project_home, artifact_source_path, property_deploy_path,
-                                                         property_file_name)
-                source_file_contents = dict(line.strip().split('=') for line in open(property_source_full_path))
-                deploy_file_contents = dict(line.strip().split('=') for line in open(property_deploy_full_path))
-                #
-                # for property_index in range(len(project_data)):
-                #     print(artifact_data[artifact_index]['properties'])
-                #     print('yyy')
-# name = parsedXml.get('project_0').get('name')
-# project_home = parsedXml.get('project_0').get('project-home')
-# branch = parsedXml.get('project_0').get('branch')
-# artifact = parsedXml.get('project_0').get('artifacts').get('artifact_0')
-#
-# print('Pulling changes of project ' + name + ' ' + branch + ' branch')
-# os.chdir(project_home)
-#
-# subprocess.call(["git fetch --all"], shell=True)
-# os.system('git checkout ' + branch)
-#
-# artifact_source_path = artifact.get('source-path')
-# artifact_deploy_path = artifact.get('deploy-path')
-# artifact_type = artifact.get('type')
-# artifact_name = artifact.get('name')
-# artifact_properties = artifact.get('properties')
-#
-# print('Building ' + artifact_name)
-#
-# os.chdir(artifact_source_path)
-#
-# H = dict(line.strip().split('=') for line in open(project_home + '/' + artifact_source_path))
-#
-# # subprocess.call(["mvn clean install"], shell=True)
-#
-# print(artifact)
+                if type == 'jar':
+                    property_deploy_full_path = property_deploy_path
+                else:
+                    property_deploy_full_path = os.path.join(deploy_dir, artifact_deploy_path, property_deploy_path,
+                                                             property_file_name)
+
+                source_file_contents = dict(line.strip().split('=', 1) for line in open(property_source_full_path))
+                deploy_file_contents = dict(line.strip().split('=', 1) for line in open(property_deploy_full_path))
+
+                updated_properties = ''
+                is_keys_updated = False
+                for key in source_file_contents:
+                    if key in deploy_file_contents:
+                        updated_properties = updated_properties + key + ' = ' + deploy_file_contents[key] + '\n'
+                    else:
+                        is_keys_updated = True
+                        updated_properties = updated_properties + key + ' = ' + source_file_contents[key] + '\n'
+                        logging.info(' (+) ' + key + ' = ' + source_file_contents[key] + ' => ' + property_file_name)
+
+                property_file = open(property_source_full_path, 'w')
+                property_file.write(updated_properties)
+                property_file.close()
+
+        path_to_artifact = os.path.join(project_home, artifact_source_path)
+        os.chdir(path_to_artifact)
+
+        subprocess.call(["mvn clean install"], shell=True)
+
+        artifact_deploy_full_path = os.path.join(deploy_dir, artifact_deploy_path)
+        print('Copying ' + artifact_final_name + '-' + artifact_version + ' to ' + artifact_deploy_full_path)
+
+        if artifact_version is None or artifact_version == '':
+            os.system('cp ' + os.path.join('target', artifact_final_name + '.' + artifact_type)
+                      + ' ' + artifact_deploy_full_path)
+        else:
+            os.system('cp ' + os.path.join('target', artifact_final_name + '-' + artifact_version + '.' + artifact_type)
+                      + ' ' + artifact_deploy_full_path)
+
